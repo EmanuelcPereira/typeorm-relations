278d29190057f3e958cda5cd031ac3c17f289f52
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-03-01 13:45:48 -0300

    All tests passed

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -15,7 +15,7 @@ container.registerSingleton<ICustomersRepository>(
 );
 
 container.registerSingleton<IProductsRepository>(
-  'ProductsReository',
+  'ProductsRepository',
   ProductsRepository,
 );
 
